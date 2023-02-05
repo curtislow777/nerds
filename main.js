2accee3af6676753cdf98a6c6140f32ef2c7c547
dhensha <S10240369@connect.np.edu.sg>
2023-02-05 23:22:49 +0800

    commit 16

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
 
-const inputs = document.querySelectorAll(".input");
 
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
-}
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
-}
-
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
 });
