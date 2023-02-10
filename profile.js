c693d638d595093d033dec2a4801f4782ba9de06
curtislow777 <116061120+curtislow777@users.noreply.github.com>
2023-02-11 03:08:56 +0800

    comment feature

--- profile.js
+++ profile.js
@@ -1,19 +1,30 @@
-var post= document.getElementById("post");
-post.addEventListener("click",function(e){
+function postComment() {
+    //Get comment
+    var userComment = document.getElementById("comment-box").value;
+  
+    var html = '<div class="list">' +
+      '<div class="user">' +
+      '<div class="user-image"><img src="./images/Animal-Crossing-Tom-Nook-with-Money.jpg"></div>' +
+      '<div class="user-name">' +
+      '<div class="name">Tom Nook</div>' +
+      '<div class="day">1 second ago</div>' +
+      '</div>' +
+      '</div>' +
+      '<div id="new-comment">' + userComment + '</div>' +
+      '</div>';
+  
+  
+    var template = document.getElementsByTagName("template")[0];
+    template.insertAdjacentHTML("afterend", html);
+  
+    //reset the comment box
+    document.getElementById("comment-box").value = "";
+  }
+  
+  var post = document.getElementById("post");
+
+  post.addEventListener("click", function(e) {
     e.preventDefault();
-    console.log('coment button pressed');
+    postComment()
 
-    var userComment=document.getElementById("comment-box").value;
-    console.log(userComment)
-
-    var text= document.createTextNode(userComment);
-    console.log(text)
-
-    var newComment = document.getElementById("new-comment");
-    console.log(newComment)
-
-    newComment.appendChild(userComment)
-    
-
-
-})
+  })
