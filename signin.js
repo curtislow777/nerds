8100a0b4880f7368d53bab6c30d04d6af685dde5
curtislow777 <116061120+curtislow777@users.noreply.github.com>
2023-02-14 10:43:23 +0800

    fix login bug

--- signin.js
+++ signin.js
@@ -1,18 +1,12 @@
-//[STEP 1]: Create our submit form listener for the login form
 $("#login-submit").on("click", function (e) {
-  //prevent default action of the button
   e.preventDefault();
-  console.log('login button pressed')
-  
-  //[STEP 2]: let's retrieve form data
-  //for now we assume all information is valid
-  //you are to do your own data validation
+  console.log('login button pressed');
+
   let email = $("#login-email").val();
   let password = $("#login-password").val();
   console.log("Email", email);
   console.log("Password", password);
-  
-  //[STEP 3]: Create our AJAX settings to retrieve the account from RESTDB based on the entered email
+
   var settings = {
     "async": true,
     "crossDomain": true,
@@ -25,16 +19,22 @@ $("#login-submit").on("click", function (e) {
     },
     "processData": false,
   }
+
   $.ajax(settings).done(function(response) {
     console.log("Response of matched data is", response);
-    
-    //[STEP 4]: Check if the retrieved account exists and the password matches
-    if (response.length > 0 && response[0].password === password) {
-      // Login successful, redirect or perform other desired action
+
+    let accountFound = false;
+    for (let i = 0; i < response.length; i++) {
+      if (response[i].email === email && response[i].password === password) {
+        accountFound = true;
+        break;
+      }
+    }
+
+    if (accountFound) {
       console.log("Login successful");
     } else {
-      // Login failed, display error message or perform other desired action
       console.error("Login failed, email or password incorrect");
     }
   });
-  })
+})
