a05d383ff77a2cb62aed59b000ceeedd375dc94b
curtislow777 <116061120+curtislow777@users.noreply.github.com>
2023-02-20 16:53:04 +0800

    fix new comment box format

--- profile.js
+++ profile.js
@@ -3,16 +3,15 @@ function postComment() {
     var userComment = document.getElementById("comment-box").value;
   
     var html = '<div class="list">' +
-      '<div class="user">' +
-      '<div class="user-image"><img src="images/avatar-2.png"></div>' +
-      '<div class="user-name">' +
-      '<div class="name">Tom Nook</div>' +
-      '<div class="date">1 second ago</div>' +
-      '</div>' +
-      '</div>' +
-      '<div id="new-comment">' + userComment + '</div>' +
-      '</div>';
-  
+                  '<div class="comment">' +
+                    '<div class="user-image"><img src="./images/avatar-2.png"></div>' +
+                    '<div class="user-details">' +
+                      '<div class="user-name">Tom Nook</div>' +
+                      '<div class="user-comment">' + userComment +'</div>' +
+                    '</div>' +
+                    '<div class="date">1 second ago</div>' +
+                  '</div>' +
+                '</div>';
   
     var template = document.getElementsByTagName("template")[0];
     template.insertAdjacentHTML("afterend", html);
